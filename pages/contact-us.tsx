f9678760bdfc2402d9664d620cfba8df7edaa9a8
RafaelGoulartB <rafagoulartb@gmail.com>
2020-10-14 09:24:39 -0300

    📦 feat: add contact us section

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
-import { Text, Image } from '@chakra-ui/core'
+import {
+  Text,
+  Image,
+  Flex,
+  FormControl,
+  Input,
+  Button,
+  Textarea
+} from '@chakra-ui/core'
 import HeroContainer from '../components/hero/hero-container'
 import HeroBreadcrumb from '../components/hero/hero-breadcrumb'
 import HeroTexts from '../components/hero/hero-texts'
@@ -24,6 +32,68 @@ const ContactUs: React.FC = () => {
           mr="8"
         />
       </HeroContainer>
+
+      <Flex
+        flexDir="column"
+        px={['8', '8', '32', '16', '0']}
+        alignItems="center"
+        pt={['32', '32', '32', '8', '32']}
+      >
+        <Text pb="12" color="#333">
+          Please note that all message will be replied within the next 12hours
+        </Text>
+
+        <form>
+          <FormControl
+            as="fieldset"
+            display="flex"
+            flexDir="row"
+            flexWrap="wrap"
+            flexGrow={1}
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Input
+              id="fname"
+              placeholder="First name"
+              flexBasis="330px"
+              flexGrow={1}
+              minH="60px"
+              mb={['6', '6', '0', '0', '0']}
+              mr={['0', '0', '0', '6', '6']}
+            />
+            <Input
+              type="email"
+              id="email"
+              flexGrow={1}
+              placeholder="Email"
+              flexBasis="330px"
+              minH="60px"
+            />
+          </FormControl>
+          <FormControl mt="6">
+            <Textarea
+              placeholder="Your Content"
+              size="lg"
+              resize="vertical"
+              minH="180px"
+            />
+          </FormControl>
+          <Button
+            variantColor="pink"
+            background="linear-gradient(178.18deg, #FD749B -13.56%, #281AC8 158.3%);"
+            color="white"
+            rounded="100px"
+            width="158px"
+            mt="16"
+            mb="6"
+            py="6"
+            textTransform="uppercase"
+          >
+            Publish
+          </Button>
+        </form>
+      </Flex>
     </>
   )
 }
